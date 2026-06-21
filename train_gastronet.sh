a67ebc8ede9bdbcd267713e4cf76a3bff523b9cb
Aniket Patil <aniketpatil6919@gmail.com>
2026-06-21 22:29:34 +0530

    Added another layer

--- train_gastronet.sh
+++ train_gastronet.sh
@@ -43,6 +43,7 @@ env PYTHONPATH=src python bin/run_patchcore.py \
   results \
   patch_core \
   -b gastronet \
+  -le blocks.9 \
   -le blocks.11 \
   --faiss_on_gpu \
   --pretrain_embed_dimension 768 \
